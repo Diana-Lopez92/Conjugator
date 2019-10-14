1fc48519156ecee817741f32d1a7af74ddf6109a
Diana <diana@example.com>
2019-10-14 18:31:04 -0500

    C2 Prueba de Funcionamiento del Request con POST

--- Python/app.py
+++ Python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from flaskext.mysql import MySQL
 
 #### Para iniciar el servidor/To Start the server
@@ -15,7 +15,7 @@ mysql.init_app(app)
 
 
 @app.route('/')
-def Index():
+def test():
     # Se crea la conexion de MySQL / MySQL connection is created
     conn = mysql.connect()
     # Se crea un cursor / A cursor is created
@@ -25,6 +25,18 @@ def Index():
     cursor.close()
     print(data)
     return render_template('presente.html')
+
+@app.route('/evaluar', methods=['POST'])
+def evaluar():
+    if request.method == 'POST':
+        je = request.form['je']
+        tu = request.form['tu']
+        il = request.form['il']
+        nous = request.form['nous']
+        vous = request.form['vous']
+        ils = request.form['ils']
+        print('Los valores del request son: \nje: ' + je + '\ntu: ' + tu + '\nil: ' + il + '\nnous: ' + nous + '\nvous: ' + vous + '\nils: ' + ils)
+        return render_template('presente.html')
 
 
 if __name__ == '__main__':
